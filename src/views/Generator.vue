b7905a922cc88f4ee2daedb0b8145dd392bf897e
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-11-06 13:43:27 +0900

    initial Commit

---
+++ src/views/Generator.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    #Generator
+        h1 aa
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Generator extends Vue {
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+    #home
+        p(style="white-space: pre-line;") {{ msg }}
+        br
+        textarea(v-model="msg" type="text")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Generator from '@/views/Generator.vue';
 
 @Component({
   components: {
-    HelloWorld,
+      Generator,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    public msg = 'あああああああ';
+}
 </script>
