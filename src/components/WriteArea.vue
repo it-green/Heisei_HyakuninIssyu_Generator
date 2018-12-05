a4b28e0d1e3abfad19d6da0453dba2eb5c36c8d1
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-12-05 13:31:45 +0900

    add buefy

--- src/components/WriteArea.vue
+++ src/components/WriteArea.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
-    #WriteArea
-        p.song {{ song }}
-        br
-        textarea(v-model="song" type="text")
+.WriteArea
+    //- buefyのテスト
+    //- button.button.is-info.test(@click="alert") ここを押してね
+
+
 </template>
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    @Component
+import { Component, Vue } from 'vue-property-decorator';
+import Buefy from 'buefy';
+Vue.use(Buefy);
+@Component
 
-    export default class WriteArea extends Vue {
-        public song = '';
-    }
+export default class WriteArea extends Vue {
+// buefyのテスト
+//     private alert() {
+//         this.$dialog.alert('this is test');
+//     }
+}
 </script>
 <style lang="sass">
+@import '~bulma/bulma'
+@import '~buefy/src/scss/buefy'
 .song
     p
     color: green
+button.test
+    margin-top: 100px
 </style>
