705d9354e39d81d48b27eec2e67bbdd4c8e0390c
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-12-10 14:40:50 +0900

    merge mr brown code check

--- src/components/WriteArea.vue
+++ src/components/WriteArea.vue
@@ -4,7 +4,18 @@
         p {{ upperOne }}
         p {{ upperTwo }}
 
-        b-field#sentenceOne(label="5" horizontal v-bind:type ="{'is-danger':checkUpperOne}")
+        b-checkbox(v-model="test")
+        //- FIXME: ブラウン: Buefyのサンプル通りではあるのだがエラーが出るので修正が必要
+        //- typeが要求している値の型がStringなのに対し、与えられている値の方がObjectであるのでエラーとなる
+        //- string を渡してあげる必要がある
+        //- example1: 三項演算子を利用
+        b-field#sentenceOne(label="5" horizontal  :type="test ? 'is-danger' : ''")
+            b-input#test(type="text" v-model="upperOne"  maxlength="5" @blur="first")
+        //- example2: メソッド（関数）を利用。この場合stringを返り値とするメソッド
+        b-field#sentenceOne(label="5" horizontal  :type="returnFieldType")
+            b-input#test(type="text" v-model="upperOne"  maxlength="5" @blur="first")
+
+        b-field#sentenceOne(label="5" horizontal  :type="{ 'is-danger': test }")
             b-input#test(type="text" v-model="upperOne"  maxlength="5" @blur="first")
         b-field(label="5" horizontal v-bind:type ="{'is-danger':checkUpperTwo}")
             b-input(type="text" v-model="upperTwo" maxlength="7" @blur="second")
@@ -12,7 +23,8 @@
             b-input
 
         p#takeMe(align="") 属性をとって欲しい!
-
+        //- TODO: ブラウン: refを用いてHTML要素へアクセスするためのサンプル
+        canvas(ref='canvas')
         //- このボタンを押すと#takeMeのalignがcenterになる。
         button.button(@click="setAttribute()") 属性を取る
         button.button.is-success(@click="$dialog.alert('test')") ボタン
@@ -25,10 +37,12 @@ import LoadUtil from '@/utils/LoadUtil';
 
 @Component
 export default class WriteArea extends Vue {
-    private upperOne = '千早ぶる';
-    private upperTwo = '神代もきかず';
-    private checkUpperOne = false;
-    private checkUpperTwo = false;
+    private upperOne = '給食の';
+    private upperTwo = 'キャン攻撃';
+    // FIXME: ブラウン: サンプル用のプロパティであっても'test'等の命名はしてはいけない。癖になっているようなのでこれは即矯正するべし。
+    // プロパティ名にかかわらず、関数名やその他においても'それ'が持つ役割を認識可能な命名をするように心がけること。
+    // コードを書きはじめの人、英語が苦手な人のためにこのようなサイトも存在する。https://codic.jp/
+    private test = false;
     private re = /(ぁ|ぃ|ぅ|ぇ|ぉ|っ|ゃ|ゅ|ょ|ゎ|ァ|ィ|ゥ|ェ|ォ|ッ|ャ|ュ|ョ|ヮ)/;
     private kuroshiroInstance = new Kuroshiro();
 
@@ -41,11 +55,10 @@ export default class WriteArea extends Vue {
         const countOne = replaceRe.length;
         console.log(songOne);
         console.log(countOne);
-        // 音の数が多い時にはb-fiedの属性を'is-danger'にする
-        if (countOne > 5) {
-        this.checkUpperOne = true;
-        } else {
-        this.checkUpperOne = false;
+        // FIXME: ブラウン: 音が5の場合OKならば、以下のように5以上の条件としているのは筋が通らない。'5より大きい'では？
+        // 音が5以下の場合のチェックを else if などで実装しても良いが、正規表現で特定の文字数をチェックすることもできる。実装方法は任せる。
+        if (countOne >= 5) {
+            console.log('音の数が多いよ');
         }
     }
 
@@ -56,16 +69,28 @@ export default class WriteArea extends Vue {
         const countTwo = replaceRe.length;
         console.log(songTwo);
         console.log(countTwo);
-        if (countTwo > 7) {
-        this.checkUpperTwo = true;
-        } else {
-        this.checkUpperTwo = false;
-        }
     }
+    // TODO: ブラウン質問: ここでは何がしたい？実装しようとしている機能の説明を求む。
     // HTMLの属性の書き換えの例
     private takeAttribute() {
         const getElem: HTMLElement | any  = document.getElementById('takeMe');
         const check = getElem.setAttribute('align', 'center');
+
+        // FIXME: ブラウン: vueでの開発でHTML要素へアクセスする場合、refを利用する
+        // 以下はtemplateのcanvas要素へアクセスするためのサンプル。
+        const canvasElement = this.$refs.canvas as HTMLCanvasElement;
+        console.log(canvasElement);
+    }
+
+    // 引数を与える必要がある場合はgetter関数にできない。その場合は通常のメソッドのように扱う。
+    private get returnFieldType(): string {
+        if (this.test) {
+            // this.testがtrueの場合
+            return 'is-danger';
+        } else {
+            // 上記の条件以外の場合
+            return '';
+        }
     }
 
     private mounted() {
