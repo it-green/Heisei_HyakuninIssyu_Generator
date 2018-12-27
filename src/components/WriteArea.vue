5a9093dbec2c2fbb65845a9aa3a6dcb3f52785bf
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-12-27 11:24:29 +0900

    add filltext

--- src/components/WriteArea.vue
+++ src/components/WriteArea.vue
@@ -1,32 +1,29 @@
 <template lang="pug">
 .vue-write-area
     section.section
+        .sheetArea
+            canvas.canvas-style(ref="canvas" v-modl="firstSong")
 
-        canvas(ref='canvas')
-
-        p {{ firstSong }}
-        p {{ secoundSong }}
-        p {{ thirdSong }}
-        p {{ fourthSong }}
-        p {{ fifthSong }}
-
-        p.title 上の句
         //- 上の句を書き込むエリア
-        div.columns
-            //- example2: メソッド（関数）を利用。この場合stringを返り値とするメソッド
-            b-field.column(label="初句" horizontal  :type="returnFieldTypeOne"  :message="{'音の数が多いです': returnFieldTypeOne}")
-                b-input(type="text" v-model="firstSong"  maxlength="5" @blur="first")
-            b-field.column(label="二句" horizontal :type="returnFieldTypeTwo"  :message="{'音の数が多いです': returnFieldTypeTwo}")
-                b-input(type="text" v-model="secoundSong" maxlength="7" @blur="second")
-            b-field.column(label="三句" horizontal  :type="returnFieldTypeThree"  :message="{'音の数が多いです': returnFieldTypeThree}")
-                b-input(type="text" v-model="thirdSong" maxlength="5" @blur="third")
-        p.title 下の句
-        //- 下の句を書き込むエリア
-        div.columns
-            b-field.column(label="四句" horizontal :type="returnFieldTypeFour"  :message="{'音の数が多いです': returnFieldTypeFour}")
-                b-input(type="text" v-model="fourthSong" maxlength="7" @blur="fourth")
-            b-field.column(label="結び句" horizontal :type="returnFieldTypeFive"  :message="{'音の数が多いです': returnFieldTypeFive}")
-                b-input(type="text" v-model="fifthSong" maxlength="7" @blur="fifth")
+        .forms
+            p.title 上の句
+            div.columns
+                //- example2: メソッド（関数）を利用。この場合stringを返り値とするメソッド
+                b-field.column(label="初句" horizontal  :type="returnFieldTypeOne"  :message="{'音の数が多いです': returnFieldTypeOne}")
+                    b-input(type="text" v-model="firstSong"  maxlength="5" @blur="first")
+                b-field.column(label="二句" horizontal :type="returnFieldTypeTwo"  :message="{'音の数が多いです': returnFieldTypeTwo}")
+                    b-input(type="text" v-model="secoundSong" maxlength="7" @blur="second")
+                b-field.column(label="三句" horizontal  :type="returnFieldTypeThree"  :message="{'音の数が多いです': returnFieldTypeThree}")
+                    b-input(type="text" v-model="thirdSong" maxlength="5" @blur="third")
+            p.title 下の句
+            //- 下の句を書き込むエリア
+            div.columns
+                b-field.column(label="四句" horizontal :type="returnFieldTypeFour"  :message="{'音の数が多いです': returnFieldTypeFour}")
+                    b-input(type="text" v-model="fourthSong" maxlength="7" @blur="fourth")
+                b-field.column(label="結び句" horizontal :type="returnFieldTypeFive"  :message="{'音の数が多いです': returnFieldTypeFive}")
+                    b-input(type="text" v-model="fifthSong" maxlength="7" @blur="fifth")
+
+            button.button.column.is-info(@click="downloadImg" ref="download") 画像をダウンロード
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
@@ -40,11 +37,11 @@ interface Img {
 
 @Component
 export default class WriteArea extends Vue {
-    private firstSong = '千早ぶる';
-    private secoundSong = '神代もきかず';
-    private thirdSong = '龍田川';
-    private fourthSong = 'からくれなゐに';
-    private fifthSong = '水くくる';
+    private firstSong: string  = '千早ぶる';
+    private secoundSong: string = '神代もきかず';
+    private thirdSong: string = '龍田川';
+    private fourthSong: string = 'からくれなゐに';
+    private fifthSong: string = '水くくる';
 
     private textBooleanValueOne = false;
     private textBooleanValueTwo = false;
@@ -60,9 +57,13 @@ export default class WriteArea extends Vue {
     private canvas: HTMLCanvasElement | null = null;
     private ctx: CanvasRenderingContext2D | null = null;
     private pinkSheet: HTMLImageElement | null = null;
+    private ctxFirstSong: CanvasRenderingContext2D | null = null;
+    private ctxSecoundSong: CanvasRenderingContext2D | null = null;
+    private ctxThirdSong: CanvasRenderingContext2D | null = null;
+    private ctxFourthSong: CanvasRenderingContext2D | null = null;
+    private ctxFifthSong: CanvasRenderingContext2D | null = null;
     private sheetWidth = 500;
     private sheetHeight = 500;
-
 
     // 画像を読み込む
     private sheets: Img = {
@@ -70,11 +71,33 @@ export default class WriteArea extends Vue {
     };
 
     // 画像を描画する。
-    private drawImage() {
+    private drawSheet() {
         if (this.ctx === null || this.pinkSheet === null) {
             return;
         }
         this.ctx.drawImage(this.pinkSheet, 0, 0);
+    }
+
+    private drawSong() {
+        if (this.ctxFirstSong === null || this.ctxSecoundSong === null || this.ctxThirdSong === null ||
+            this.ctxFourthSong === null || this.ctxFifthSong === null) {
+            return;
+        }
+        this.ctxFirstSong.font = '25px serif';
+        this.ctxSecoundSong.font = '25px serif';
+        this.ctxThirdSong.font = '25px serif';
+        this.ctxFourthSong.font = '25px serif';
+        this.ctxFifthSong.font = '25px serif';
+        this.ctxFirstSong.fillStyle = 'white';
+        this.ctxSecoundSong.fillStyle = 'white';
+        this.ctxThirdSong.fillStyle = 'white';
+        this.ctxFourthSong.fillStyle = 'white';
+        this.ctxFifthSong.fillStyle = 'whitw';
+        this.ctxFirstSong.fillText(this.firstSong, 50, 160);
+        this.ctxSecoundSong.fillText(this.secoundSong, 50, 200);
+        this.ctxThirdSong.fillText(this.thirdSong, 50, 240);
+        this.ctxFourthSong.fillText(this.fourthSong, 50, 280);
+        this.ctxFifthSong.fillText(this.fifthSong, 50, 320);
     }
 
     private async first() {
@@ -173,15 +196,29 @@ export default class WriteArea extends Vue {
             return '';
         }
     }
+    // 画像をbase64形式に変換する。
+    private downloadImg() {
+        if (this.canvas === null) {
+            return;
+        }
+        const typeBase64 =  this.canvas.toDataURL('image/png');
+        console.log(typeBase64);
+    }
     private mounted() {
         LoadUtil.loading(this.$loading, async () => {
             await this.kuroshiroInstance.init(new KuromojiAnalyzer({dictPath: '/dict'}));
             console.log('kuroshiro ready');
         });
+        // canvasの処理(画像の方)
         this.canvas = this.$refs.canvas as HTMLCanvasElement;
         this.canvas.width = this.sheetWidth;
         this.canvas.height = this.sheetHeight;
         this.ctx = this.canvas.getContext('2d');
+        this.ctxFirstSong = this.canvas.getContext('2d');
+        this.ctxSecoundSong = this.canvas.getContext('2d');
+        this.ctxThirdSong = this.canvas.getContext('2d');
+        this.ctxFourthSong = this.canvas.getContext('2d');
+        this.ctxFifthSong = this.canvas.getContext('2d');
 
         this.pinkSheet = new Image();
 
@@ -189,15 +226,27 @@ export default class WriteArea extends Vue {
             if (this.ctx === null || this.pinkSheet === null) {
                 return;
             }
-            this.drawImage();
+            this.drawSheet();
+            this.drawSong();
         };
         this.pinkSheet.src = this.sheets['pink-sheet'];
     }
 }
 </script>
 <style lang="sass">
+@import 'all';
 .vue-write-area
     .song
         p
         color: green
+    .sheetArea
+        display: flex
+    .canvas-style
+        margin: 0 auto
+    // スマホ用のcss
+    @media screen and (max-width: $tablet)
+        .canvas-style
+            margin: 0 auto
+            height: 100%
+            width: 100%
 </style>
