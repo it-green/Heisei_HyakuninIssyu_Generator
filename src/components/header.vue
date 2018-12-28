c790c56ded2337456845f39e379e9ebb3f181ef7
Ryotaro Kato <ryo.ctuad@gmail.com>
2018-12-28 20:32:46 +0900

    add new image

---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class Header extends Vue {
+}
+</script>
+
+<style lamg="sass">
+</style>
